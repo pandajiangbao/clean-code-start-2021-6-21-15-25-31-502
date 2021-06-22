ecac2d0658b71607de73a263eb798bd929747927
Panda <wupa2@oocl.com>
2021-06-22 15:55:32 +0800

    $6:add getter and setter

--- src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/Address.java
+++ src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/Address.java
@@ -5,4 +5,22 @@ package com.tw.academy.basic.$6_primitive_obsession.practiceOne;
  * @date 6/22/2021
  */
 public class Address {
+    private String province;
+    private String city;
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
