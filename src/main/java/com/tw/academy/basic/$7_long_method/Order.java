4cf74deccc48ccead6acccfa7f23e1f2535d4652
Panda <wupa2@oocl.com>
2021-06-22 19:30:19 +0800

    $7:inline Order.java filed

--- src/main/java/com/tw/academy/basic/$7_long_method/Order.java
+++ src/main/java/com/tw/academy/basic/$7_long_method/Order.java
@@ -4,23 +4,19 @@ import java.util.List;
 
 public class Order {
     private final Customer customer;
-    private final String customerName;
-    private final String address;
     List<LineItem> lineItems;
 
     public Order(Customer customer, List<LineItem> lineItems) {
         this.customer = customer;
-        this.customerName = this.customer.getCustomerName();
-        this.address = this.customer.getAddress();
         this.lineItems = lineItems;
     }
 
     public String getCustomerName() {
-        return customerName;
+        return this.customer.getCustomerName();
     }
 
     public String getCustomerAddress() {
-        return address;
+        return this.customer.getAddress();
     }
 
     public List<LineItem> getLineItems() {
